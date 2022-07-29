dd795b453915146f2b66dd12b086cba3a7c0f701
Lokeshny677 <lokeshny677@gmail.com>
2022-07-29 19:55:07 +0530

    Added present or absent using random

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,14 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage calculation");
+        int FULL_TIME = 0;
+        int empType = (int) (Math.random() * 100) % 2;
+            if (empType == FULL_TIME) {
+                System.out.println("Employee is Present");
+            }
+            else
+            {
+                System.out.println("Employee is Absent");
+            }
     }
 }
